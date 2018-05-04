010c7253b2993e49384a4d6e3338fd9b2caac127
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-05-04 10:17:49 +0200

    Tutorial 7/d - Add a dashboard view

---
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DashboardComponent
+      ],
+      providers: [
+        HeroService,
+        MessageService
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
