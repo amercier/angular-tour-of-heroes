3c44d1a4823ee867d7fbe12095eabce926544d1c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-05-04 10:09:08 +0200

    Tutorial 7/b - Add RouterOutlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 @Component({ selector: 'app-heroes', template: '' })
@@ -15,6 +16,9 @@ describe('AppComponent', () => {
         AppComponent,
         HeroesComponentStub,
         MessagesComponentStub
+      ],
+      imports: [
+        RouterTestingModule
       ],
     }).compileComponents();
   }));
