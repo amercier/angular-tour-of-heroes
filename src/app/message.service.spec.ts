09c0183b93047e541b7720640508586e7c7074a6
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-05-09 15:51:33 +0200

    Unit tests for MessageService

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -12,4 +12,25 @@ describe('MessageService', () => {
   it('should be created', inject([MessageService], (service: MessageService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should be created with an empty messages list', inject([MessageService], (service: MessageService) => {
+    expect(service.messages).toEqual([]);
+  }));
+
+  describe('#add', () => {
+    it('should add the given message to the list', inject([MessageService], (service: MessageService) => {
+      service.add('foo');
+      service.add('bar');
+      expect(service.messages).toEqual(['foo', 'bar']);
+    }));
+  });
+
+  describe('#clear', () => {
+    it('should empty the message list', inject([MessageService], (service: MessageService) => {
+      service.add('foo');
+      service.add('bar');
+      service.clear();
+      expect(service.messages).toEqual([]);
+    }));
+  });
 });
