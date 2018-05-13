1099d1c2d8e09b8ab4ca73a1cf04e82d81d66d0f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-05-13 23:36:25 +0200

    Unit tests for MessageComponent

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 
 import { MessagesComponent } from './messages.component';
 import { MessageService } from '../message.service';
@@ -7,25 +7,80 @@ describe('MessagesComponent', () => {
   let component: MessagesComponent;
   let fixture: ComponentFixture<MessagesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        MessagesComponent
-      ],
-      providers: [
-        MessageService
-      ]
-    })
-    .compileComponents();
+  /**
+   * Configure `TestBed`
+   * @param messageServiceStub Message service or mock
+   * @return Promise
+   */
+  function configureTestingModule(messageServiceStub = { messages: [] }) {
+    return TestBed
+      .configureTestingModule({
+        declarations: [MessagesComponent],
+        providers: [
+          { provide: ComponentFixtureAutoDetect, useValue: true },
+          { provide: MessageService, useValue: messageServiceStub },
+        ]
+      })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(MessagesComponent);
+        component = fixture.componentInstance;
+      });
+  }
+
+  it('should create', () => configureTestingModule().then(() => {
+    expect(component).toBeTruthy();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MessagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  describe('when message service contains messages', () => {
+    const messages = ['foo', 'bar'];
+    beforeEach(() => configureTestingModule({ messages }));
+
+    it('should display a "Messages" title', () => {
+      const titleElement = fixture.nativeElement.querySelector('h2');
+      expect(titleElement).toBeDefined();
+      expect(titleElement.textContent).toBe('Messages');
+    });
+
+    it('should display a "clear" button', () => {
+      const clearButtonElement = fixture.nativeElement.querySelector('button.clear');
+      expect(clearButtonElement).toBeDefined();
+      expect(clearButtonElement.textContent).toBe('clear');
+    });
+
+    it('should display messages from the message service', () => {
+      const messageElements = fixture.nativeElement.querySelector('div').querySelectorAll('div');
+      expect(messageElements.length).toBe(2);
+
+      const displayedMessages = Array.from(messageElements).map((d: HTMLElement) => d.textContent);
+      expect(displayedMessages).toEqual(messages);
+    });
+
+    describe('#clear button', () => {
+      it('should empty the message list when clicked', () => {
+        component.messageService.clear = jasmine.createSpy('clear');
+
+        const clearButtonElement = fixture.nativeElement.querySelector('button.clear');
+        clearButtonElement.click();
+        expect(component.messageService.clear).toHaveBeenCalled();
+      });
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('when message service contains no message', () => {
+    const messages = [];
+    beforeEach(() => configureTestingModule({ messages: [] }));
+
+    it('should not display "Messages"', () => {
+      expect(fixture.nativeElement).not.toContain('Messages');
+    });
+
+    it('should not display any button', () => {
+      expect(fixture.nativeElement.querySelectorAll('button').length).toBe(0);
+    });
+
+    it('should not display the container element', () => {
+      expect(fixture.nativeElement.querySelector('div')).toBe(null);
+    });
   });
 });
